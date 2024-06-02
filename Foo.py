282b8f8b909496dbdc4bf42f48575ee9587cb6f0
Vinci Wu <wu.vinci00@gmail.com>
2024-06-02 17:26:32 -0400

    Removed constructor and moved the radius check into the calculate method.

--- Foo.py
+++ Foo.py
@@ -1,5 +1,11 @@
+import math
+
 class Foo:
-    def __init__(self, radius) -> None:
+    def calculate_sphere_volume(self, radius):
+        '''
+        Given the radius of a sphere, returns the volume
+        Throws exception if radius < 0
+        '''
         if radius < 0:
             raise ValueError('Radius cannot be negative')
-        self.radius = radius
+        return (4/3)*math.pi*(radius**3)
