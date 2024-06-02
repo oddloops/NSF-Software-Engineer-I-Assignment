9813e02d274fb0090206ee928b09756d2383b13d
Vinci Wu <wu.vinci00@gmail.com>
2024-06-02 16:40:11 -0400

    Created skeleton tests for calculating sphere radius

--- Foo.py
+++ Foo.py
@@ -1,3 +1,5 @@
 class Foo:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, radius) -> None:
+        if radius < 0:
+            raise ValueError('Radius cannot be negative')
+        self.radius = radius
--- Test_Foo.py
+++ Test_Foo.py
@@ -2,5 +2,12 @@ import unittest
 from Foo import *
 
 class TestFoo(unittest.TestCase):
-    def test_intialization(self):
+    def test_positive_radius(self):
         pass
+    def test_negative_radius(self):
+        pass
+    def test_zero_radius(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
