43996871f84b5c0083be3e3ae054bd58f038910b
Vinci Wu <wu.vinci00@gmail.com>
2024-06-02 17:27:18 -0400

    Created tests for a postive, negative, and zero radius

--- Test_Foo.py
+++ Test_Foo.py
@@ -1,13 +1,24 @@
 import unittest
-from Foo import *
+from foo import Foo
 
 class TestFoo(unittest.TestCase):
     def test_positive_radius(self):
-        pass
+        radius = 2.5
+        expected_volume = 65.44985
+        calculated_volume = Foo().calculate_sphere_volume(radius)
+        self.assertAlmostEqual(calculated_volume, expected_volume, places=4)
+
     def test_negative_radius(self):
-        pass
+        with self.assertRaises(ValueError):
+            radius = -1
+            negative_foo = Foo()
+            negative_foo.calculate_sphere_volume(radius)
+
     def test_zero_radius(self):
-        pass
+        radius = 0
+        expected_volume = 0
+        calculated_volume = Foo().calculate_sphere_volume(radius)
+        self.assertEqual(calculated_volume, expected_volume)
 
 if __name__ == '__main__':
     unittest.main()
