fc75b11fbea3a945cb2760a67b67560fe4b33d65
Vinci Wu <wu.vinci00@gmail.com>
2024-06-02 17:49:29 -0400

    Added constructor to create a instance variable for PI and added an explanation behind my implementation reasoning

--- Foo.py
+++ Foo.py
@@ -1,6 +1,9 @@
 import math
 
 class Foo:
+    def __init__(self) -> None:
+        self.PI = math.pi
+
     def calculate_sphere_volume(self, radius):
         '''
         Given the radius of a sphere, returns the volume
@@ -8,4 +11,16 @@ class Foo:
         '''
         if radius < 0:
             raise ValueError('Radius cannot be negative')
-        return (4/3)*math.pi*(radius**3)
+        return (4/3)*self.PI*(radius**3)
+    
+'''
+Explanation
+I decided to take an Object Oriented approach as this software package needs to be flexible and extendable.
+Using an OOP approach allows me to utilize the Open/Close principle which will allow for the desire flexbility and continued contribution.
+I decided to create a utility class since this package is expected to be used for complex calculations. 
+Other developers will therefore be able to define new methods within the class that suits their needs.
+I defined a method, "calculate_sphere_volume", to take a radius and return the volume. It contains a simple check to ensure the radius is valid before executing the calculation.
+
+Assumptions & Expectations:
+- input for calculate_sphere_volume will always be a number, i.e. "2" will NOT be an input
+'''
